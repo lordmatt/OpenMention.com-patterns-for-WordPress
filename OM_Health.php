f3cd01f2401bbc5cd625793694009a4cbdf1b6f8
Matthew <thanetstar@gmail.com>
2025-08-03 15:32:35 +0100

    Update OM_Health.php
    added meta tag for health

--- OM_Health.php
+++ OM_Health.php
@@ -3,7 +3,7 @@
 Plugin Name: Open Mention Health Tags as Block Patters
 Plugin URI: https://github.com/lordmatt/OpenMention.com-patterns-for-WordPress
 Description: Adds a pattern for tagging OpenTopic health tags
-Version: 1.0
+Version: 1.1
 Author: Matt
 Author URI: https://openmentions.com/
 License: GPL2
@@ -32,6 +32,10 @@ function OT_OM_health_block_pattern() {
     register_block_pattern_category( 'mentalhealth', array( 
         'label'       => __( 'Mental Health', 'OT_OM' ),
         'description' => __( 'Mental health related content.', 'OT_OM' )
+    ) );       
+    
+    register_block_pattern_category( 'meta', array( 
+        'label'       => __( 'Meta', 'OT_OM' )
     ) );    
     
     register_block_pattern( 
@@ -57,10 +61,19 @@ function OT_OM_health_block_pattern() {
     ); 
     
     
-    
-    
-}
- 
+    register_block_pattern( 
+	'opentopic/healthtagmeta',
+	array(
+		'title'      => 'OpenMention: Health meta',
+		'categories' => ['tags','health','text','webmention','meta'],
+		'content'    => '<!-- wp:paragraph -->
+<p><a href="https://openmentions.com/topics/health/mental-health/">#MentalHealth</a></p>
+<!-- /wp:paragraph -->',
+	)   
+    );    
+
+} 
 add_action( 'init', 'OT_OM_health_block_pattern' );
+
 
 
