89d1f9219cf47a82083df59eaabd5eb4785eaf63
Matthew <thanetstar@gmail.com>
2025-08-03 15:51:12 +0100

    Update OM_dreamlog.php
    Correcting a silly mistake

--- OM_dreamlog.php
+++ OM_dreamlog.php
@@ -3,7 +3,7 @@
 Plugin Name: Open Mention Dream Tags as Block Patters
 Plugin URI: https://github.com/lordmatt/OpenMention.com-patterns-for-WordPress
 Description: Adds patterns for OpenMention dream logging and meta webmentions
-Version: 1.0
+Version: 1.01
 Author: Matt
 Author URI: https://openmentions.com/
 License: GPL2
@@ -35,18 +35,29 @@ function OT_OM_dreamlog_block_pattern() {
     register_block_pattern( 
 	'opentopic/dreamlog',
 	array(
-		'title'      => 'OpenMention: #health',
+		'title'      => 'OpenMention: #dreamlog',
 		'categories' => ['tags','text','webmention','dreams'],
 		'content'    => '<!-- wp:paragraph -->
 <p><a href="https://openmentions.com/topics/dream-log/">#DreamLog</a></p>
 <!-- /wp:paragraph -->',
 	)   
-    );
+    );  
     
+    register_block_pattern( 
+	'opentopic/dreamlog',
+	array(
+		'title'      => 'Add this one to the Dream Log',
+		'categories' => ['tags','text','webmention','dreams'],
+		'content'    => '<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right"><em>Add this one to the <a href="https://openmentions.com/topics/dream-log/">Dream Log</a>.</em></p>
+<!-- /wp:paragraph -->',
+	)   
+    );
+     
     register_block_pattern( 
 	'opentopic/dreamlogmeta',
 	array(
-		'title'      => 'OpenMention: #MentalHealth',
+		'title'      => 'OpenMention: #DreamLogMeta',
 		'categories' => ['tags','text','meta','webmention'],
 		'content'    => '<!-- wp:paragraph -->
 <p><a href="https://openmentions.com/topics/dream-log/meta-dream-log/">#DreamLogMeta</a></p>
@@ -58,7 +69,7 @@ function OT_OM_dreamlog_block_pattern() {
    
 
 } 
-add_action( 'init', 'OT_OM_dreamlog_block_pattern' );
 
+add_action( 'init', 'OT_OM_dreamlog_block_pattern' );
 
 
